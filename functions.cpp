c306ddb88a5ab91cd988e088c049de9e3a808b12
劉彥麟 <andy@AndyMac.local>
2015-05-01 01:27:51 +0800

    add functions.cpp

---
+++ functions.cpp
@@ -0,0 +1,15 @@
+//
+//  functions.pch
+//  back
+//
+//  Created by 劉彥麟 on 2015/4/30.
+//  Copyright (c) 2015年 劉彥麟. All rights reserved.
+//
+
+#ifndef back_functions_pch
+#define back_functions_pch
+
+// Include any system framework and library headers here that should be included in all compilation units.
+// You will also need to set the Prefix Header build setting of one or more of your targets to reference this file.
+
+#endif
